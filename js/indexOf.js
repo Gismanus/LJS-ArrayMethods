fb52e37d12a345ae6f20335d6e6da203416d854a
Michail <Gismanus@gmail.com>
2022-10-15 22:24:01 +0300

    method indexOf

---
+++ js/indexOf.js
@@ -0,0 +1,10 @@
+let arr = [1, 0, false];
+console.log(arr.indexOf(1));
+console.log(arr.indexOf(null));
+console.log(arr.includes(0));
+let fruits = ['Apple', 'Orange', 'Apple'];
+console.log(fruits.indexOf('Apple'))
+console.log(fruits.lastIndexOf('Apple'))
+const arrNaN = [NaN];
+console.log(arrNaN.indexOf(NaN));
+console.log(arrNaN.includes(NaN));
