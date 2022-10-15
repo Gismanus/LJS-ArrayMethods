72b370e17062c535c6b07358c65672a6e1da0553
Michail <Gismanus@gmail.com>
2022-10-15 07:04:07 +0300

    some stranges with arr.unshift

---
+++ js/splice.js
@@ -0,0 +1,8 @@
+let arr = ['I', 'go', 'home'];
+
+delete arr[1];
+
+console.log(arr[1]);
+
+console.log(arr);
+console.log(arr.length)
---
+++ js/test.js
@@ -0,0 +1,5 @@
+let fruits = [2, "Orange", "Pear", 2];
+
+console.log(fruits.unshift(2)); // some stranges
+
+console.log(fruits);
