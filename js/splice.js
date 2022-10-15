621115e94841d09805859101233752ba495ee1e7
Michail <Gismanus@gmail.com>
2022-10-15 07:24:01 +0300

    splice method

--- js/splice.js
+++ js/splice.js
@@ -1,8 +1,17 @@
-let arr = ['I', 'go', 'home'];
+// let arr = ['I', 'study', 'JavaScript', 'right', 'now'];
 
-delete arr[1];
+//arr.splice(1, 1); // from index 1 remove 1
 
-console.log(arr[1]);
-
+/* let removed = arr.splice(0, 3, "Let's", "dance"); // from index 0 remove 3 and add "Let's" & "dance"
 console.log(arr);
-console.log(arr.length)
+console.log(removed); */
+
+/* arr.splice(2, 0, 'complex', 'language');
+console.log(arr) */
+
+// negative indexes are allowed
+
+let arr = [1, 2, 5];
+
+arr.splice(-1, 0, 3, 4);
+console.log(arr)
