d1488697a74306816a41aaba0183fc8669ba5551
Michail <Gismanus@gmail.com>
2022-10-17 21:29:35 +0300

    reverse task fixed

--- js/tasks/reverse.js
+++ js/tasks/reverse.js
@@ -1,9 +1,5 @@
 let arr = [5, 2, 1, -10, 10, 8];
 
-function compareNumeric(a, b) {
-    return a > b ? 1 : a == b ? 0 : a < b ? -1 : true;
-}
-
-arr.sort(compareNumeric);
+arr.sort((a, b) => b - a);
 
 console.log(arr);
