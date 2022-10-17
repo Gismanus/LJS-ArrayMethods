417d1e22432e4104369afd90efb83f009f7b6ec7
Michail <Gismanus@gmail.com>
2022-10-17 21:35:41 +0300

    copySorted

---
+++ js/tasks/copySorted.js
@@ -0,0 +1,14 @@
+let arr = [
+    'HTML',
+    'JavaScript',
+    'CSS',
+]
+
+function copySorted(arr) {
+    return arr.slice().sort();
+}
+
+let sorted = copySorted(arr);
+
+console.log(arr);
+console.log(sorted);
