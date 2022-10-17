bb6480d6a530c55507f68d50e462d5e946a58132
Michail <Gismanus@gmail.com>
2022-10-18 01:36:21 +0300

    array of objects : array of names

---
+++ js/tasks/transformToNamesArray.js
@@ -0,0 +1,8 @@
+let vasya = {name: 'Vasya', age: 25};
+let petya = {name: 'Petya', age: 30};
+let masha = {name: 'Masha', age: 28};
+
+let users = [vasya, petya, masha];
+
+let names = users.map(name => name.name);
+console.log(names);
